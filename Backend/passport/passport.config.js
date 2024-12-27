b11685f4ab2464b3092245f9140b17fc088e7240
Rohan Shrestha <nikeshshrestha405@gmail.com>
2024-12-27 23:29:20 +0545

    updating user crendintals

--- Backend/passport/passport.config.js
+++ Backend/passport/passport.config.js
@@ -1,6 +1,6 @@
 import passport from "passport";
 import bcrypt from "bcryptjs"
-import user from "../../models/user.model.js";
+import user from "./models/user.model.js";
 import { GraphQLLocalStrategy } from "graphql-passport";
 
 export const configurePassport = async () => {
@@ -10,8 +10,8 @@ export const configurePassport = async () => {
 
     passport.deserializeUser(async(id,done) => {
         try {
-            const user = await user.findById(id)
-            done(null,user)
+            const User = await user.findById(id)
+            done(null,User)
         } catch (error) {
             done(error)
         }
@@ -20,9 +20,9 @@ export const configurePassport = async () => {
     passport.use(
         new GraphQLLocalStrategy(async(username,password,done) => {
             try {
-                const user = await user.findOne({username})
+                const User = await user.findOne({username})
                 const validPassword = await bcrypt.compare(password, user.password) 
-                if(!user || !validPassword){
+                if(!User || !validPassword){
                     throw new Error("Invalid Username or password")
                 }
                 return done(null,user)
--- Backend/resolvers/user.resolvers.js
+++ Backend/resolvers/user.resolvers.js
@@ -1,12 +1,32 @@
 import { users } from "../dummyData/data.js"
-
+import user from "../models/user.model.js"
+import bcrypt from "bcryptjs"
 const userResolvers = {
+    mutation:{
+        signUp: async(_,{input}, context) => {
+            try {
+                const {username,name,password,gender} = input
+                if(!username||!password||!name||!gender){
+                    throw new Error ("Field Shouldnt be empty")
+                }
+                const existingUser = await user.findOne({username})
+                if(existingUser){
+                    throw new Error("User already exists")
+                }
+
+                const salt = await bcrypt.genSalt(10)
+                const hashedPassword = await bcrypt.hash(password,salt)
+                
+            } catch (error) {
+                
+            }
+        }
+    },
     Query: {
         users: ()=>{
             return users
         }
     },
-    mutation:{}
 }
 
 export default userResolvers
